6f14dedf58dba22391cc18ad2040a342042c420f
phoenixpan <phoenixpan1992@gmail.com>
2019-10-20 19:02:26 +1100

    update js

--- Class2/index.js
+++ Class2/index.js
@@ -1,12 +1,36 @@
-console.log("Hello World")
+/* Plain javascript examples */
 
-const circle = {
-    radius:1,
-    location: {
-        x:1,
-        y:1
-    },
-    draw:function(){
-        console.log("draw");
-    }
-};
+// var nameElement = document.getElementById('board-content');
+
+// function printHello(name) {
+//     console.log('Hello ' + name);
+// }
+
+// setTimeout(() => {
+//     nameElement.innerHTML = 'Hi';
+// }, 2000);
+
+// var goBtn = document.getElementById('go-btn');
+
+// goBtn.addEventListener('click', () => {
+//     nameElement.innerHTML = 'Go';
+// });
+
+// var nameElement = jQuery('#board-content');
+// nameElement.innerHTML = 'Hi';
+
+/* jQuery examples*/
+
+// var goBtn = $('#go-btn');
+
+// goBtn.click(() => {
+//     updateName('Phoenix');
+// });
+
+// var updateName = name => {
+//     $('#name-content').html('I am ' + name);
+// };
+
+// var updateName = function(name) {
+//     $('#name-content').html('I am ' + name);
+// };
